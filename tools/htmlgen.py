a43349349353ebececd430edaa6658cb5b7d56e5
berrysauce <contact@berrysauce.me>
2021-08-02 20:53:18 +0200

    Added "Communication" category

--- tools/htmlgen.py
+++ tools/htmlgen.py
@@ -10,8 +10,8 @@ load_dotenv()
 deta = Deta(os.getenv("DETA_TOKEN"))
 toolsdb = deta.Base("whatdevsneed-posts")
 
-all_categories = ["A/B Testing", "AIs", "Analytics", "APIs", "Automation", "Backups", "Blockchain", "Blogging", "Collaboration", "Community", 
-                  "Continuous Integrations", "Databases", "Design", "Domains", "Emails", "Extensions", "Game Engines", "Hosting", "IDEs", 
+all_categories = ["A/B Testing", "AIs", "Analytics", "APIs", "Automation", "Backups", "Blockchain", "Blogging", "Collaboration", "Community",
+                  "Communication", "Continuous Integrations", "Databases", "Design", "Domains", "Emails", "Extensions", "Game Engines", "Hosting", "IDEs", 
                   "Issue Tracking", "Documentation", "Learning", "Legal", "Libraries", "Licensing", "Localization", "Logging", "Messaging", 
                   "Monitoring", "Payments", "Performance", "Productivity", "Publishing", "Security", "Software", "Storage", "Terminals", "Other"]
 
