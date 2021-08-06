e4a79cb3bc9be070d83438226f861c9d9534b273
berrysauce <contact@berrysauce.me>
2021-08-06 21:13:04 +0200

    Exchanged "A/B Testing" category with "Testing" category

--- tools/htmlgen.py
+++ tools/htmlgen.py
@@ -10,10 +10,10 @@ load_dotenv()
 deta = Deta(os.getenv("DETA_TOKEN"))
 toolsdb = deta.Base("whatdevsneed-posts")
 
-all_categories = ["A/B Testing", "AIs", "Analytics", "APIs", "Automation", "Backups", "Blockchain", "Blogging", "Collaboration", "Community",
+all_categories = ["AIs", "Analytics", "APIs", "Automation", "Backups", "Blockchain", "Blogging", "Collaboration", "Community",
                   "Communication", "Continuous Integrations", "Databases", "Design", "Domains", "Emails", "Extensions", "Game Engines", "Hosting", "IDEs", 
                   "Issue Tracking", "Documentation", "Learning", "Legal", "Libraries", "Licensing", "Localization", "Logging", "Messaging", 
-                  "Monitoring", "Payments", "Performance", "Productivity", "Publishing", "Security", "Software", "Storage", "Terminals", "Other"]
+                  "Monitoring", "Payments", "Performance", "Productivity", "Publishing", "Security", "Software", "Storage", "Terminals", "Testing", "Other"]
 
 def categorylist():
     return all_categories
